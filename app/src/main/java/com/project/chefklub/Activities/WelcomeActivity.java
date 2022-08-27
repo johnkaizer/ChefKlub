72da483a0839684d70e9ce8c5f39397dd701829e
john kaiser <52598487+johnkaizer@users.noreply.github.com>
2022-08-27 16:53:40 +0300

    Added an icon

--- app/src/main/java/com/project/chefklub/Activities/WelcomeActivity.java
+++ app/src/main/java/com/project/chefklub/Activities/WelcomeActivity.java
@@ -1,4 +1,4 @@
-package com.project.chefklub;
+package com.project.chefklub.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.project.chefklub.R;
 import com.project.chefklub.ui.login.LoginActivity;
 import com.project.chefklub.ui.registratuion.RegistrationActivity;
 
